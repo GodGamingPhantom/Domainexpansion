e6f401cb14ab8ad1ef2f429765e1842e03fa6780
GodGamingPhantom <87639891+GodGamingPhantom@users.noreply.github.com>
2024-02-19 19:18:17 +0530

    Add files via upload

---
+++ ds/scripts.js
@@ -0,0 +1,7 @@
+// This is optional and not required for the animation
+// It just adds a class to the body element when the page loads
+// to hide the content briefly before showing the coming soon animation
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("coming-soon-active");
+});
